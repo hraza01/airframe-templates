192a084dc68a9c010370d094a6a74af4af866eba
Hasan Raza <razahasan24@gmail.com>
2026-02-07 12:24:28 -0500

    chore(basic-dag): cleanup project structure
    Removed development tooling and updated DAG configuration to align with the new template structure.

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from pathlib import Path
 
 import pendulum
 from airflow import DAG
-from airflow.operators.python import ShortCircuitOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
 # fmt: off
 # isort: off
-from __PROJECT_NAME__.tasks import call_external_api, validate_api_response
 from __PROJECT_NAME__.utils import load_config, load_workflow_md
 # isort: on
 # fmt: on
@@ -54,17 +52,8 @@ def create_dag(dag_args):
         No logic outside of tasks(operators) or they are constantly run by scheduler
         """
 
-        # Trigger external API call (Airflow orchestrates, doesn't process)
-        python_task = call_external_api("https://jsonplaceholder.typicode.com/posts/1")
-
-        # Create validation task with short-circuit behavior
-        validate_task = ShortCircuitOperator(
-            task_id="validate_api_response",
-            python_callable=validate_api_response,
-        )
-
-        sql_task = BigQueryInsertJobOperator(
-            task_id="sql_task",
+        first_sql_task = BigQueryInsertJobOperator(
+            task_id="first_sql_task",
             configuration={
                 "query": {
                     "query": "{% include '/sql/example.sql' %}",
@@ -73,22 +62,18 @@ def create_dag(dag_args):
             },
         )
 
-        templated_sql_task = BigQueryInsertJobOperator(
-            task_id="templated_sql_task",
+        second_sql_task = BigQueryInsertJobOperator(
+            task_id="second_sql_task",
             configuration={
                 "query": {
-                    "query": "{% include '/sql/template.sql' %}",
+                    "query": "{% include '/sql/example-two.sql' %}",
                     "useLegacySql": False,
                 }
             },
-            # Pass upstream task ID to allow SQL template to pull XCom data
-            params={"upstream_task_id": "call_external_api"},
         )
 
         # Set task dependencies
-        # Only execute templated_sql_task if API call was successful (200)
-        python_task >> sql_task
-        python_task >> validate_task >> templated_sql_task
+        first_sql_task >> second_sql_task
 
     globals()[dag.dag_id] = dag
 
